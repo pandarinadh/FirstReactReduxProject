3cf0d91ceb06e9a255ced664f0043a7d234053a7
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-05-21 13:54:08 -0400

    update packages

--- containers/App.js
+++ containers/App.js
@@ -2,6 +2,6 @@ import React, {Component} from 'react';
 
 export default class App extends Component {
     render () {
-        return <p> this is my first react app  </p>
+        return <p> this is my first react app sss  </p>
     }
 }
