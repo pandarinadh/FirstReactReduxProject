78d3bafb79048d1df7bde543c48b51fc5afa2119
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-05-21 13:12:12 -0400

    dd

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+var parentDir =  path.join(__dirname, '../');
+
+module.exports = {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['react']
+                  }
+                }
+            ]
+        },{
+            test: /\.less$/,
+            loaders: ["style-loader", "css-loader", "less-loader" ]
+        }
+    ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+          host: 'localhost',
+          port: 8080,
+          files: ['./dist/*.html','./dist/*.js'],
+          server: { baseDir: ['dist'] }
+        })
+      ],
+    watch: true,
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
